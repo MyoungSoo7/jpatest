03921761b4607f5a272a93dfb4d7456cd00445f2
iamipro <iamipro@naver.com>
2024-09-10 06:18:28 +0900

    jpa strategy

--- src/main/java/me/lms/jpatest/objectmodel/Member.java
+++ src/main/java/me/lms/jpatest/objectmodel/Member.java
@@ -12,19 +12,27 @@ public class Member {
 
      //객체지향 쿼리 JPQL : select m from Member m where m.username = 'kim'
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "MEMBER_ID")
     private String id;
 
     private String username;
 
     // 데이터 중심 테이블은 외래키로 연관관계를 맺는다. 객체는 참조(주소)로 연관관계를 맺는다.
-    // 다대일관계 optional = false : 필수로 연관관계가 있어야 한다. fetch = FetchType.LAZY : 지연로딩 , cascade = CascadeType.ALL : 영속성 전이, targetEntity = Team.class : 연관관계의 주인    @ManyToOne
+    // 다대일관계 optional = false : 다쪽이 필수로 연관관계가 있어야 한다. fetch = FetchType.LAZY : 지연로딩 , cascade = CascadeType.ALL : 영속성 전이, targetEntity = Team.class : 연관관계의 주인    @ManyToOne
     // 왜래키를 매핑할 때 사용
+    @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     public void setTeam(Team team) {
         this.team = team;
+
+        if(!team.getMembers().contains(this)) {
+            team.getMembers().add(this);
+        }
+
+
     }
 
 
--- src/main/java/me/lms/jpatest/objectmodel/Team.java
+++ src/main/java/me/lms/jpatest/objectmodel/Team.java
@@ -1,11 +1,12 @@
 package me.lms.jpatest.objectmodel;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Setter
@@ -13,10 +14,21 @@ import lombok.Setter;
 public class Team {
 
     @Id
+    @GeneratedValue
     @Column(name = "TEAM_ID")
     private String id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+
+    public void addMember(Member member) {
+        this.members.add(member);
+        if(member.getTeam() != this) {
+            member.setTeam(this);
+        }
+    }
 
 
 }
